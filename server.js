afe6a744e0ba2580a29e226d494cd7f43fb39a6a
c4rnack <150616727+c4rnack@users.noreply.github.com>
2025-05-25 22:06:38 +0300

    return cors

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-//const cors = require('cors');
-//app.use(cors());
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
